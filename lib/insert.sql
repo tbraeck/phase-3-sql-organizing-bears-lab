98bb7b759704fbbfb766c10294ef96f39b459787
Tate <tatebraeckel@gmail.com>
2023-04-11 18:02:24 -0600

    commit tree f0ee3c2e8b0247123f804f4c8d6af44dbc387ae9

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 5, "M", "Red", "A_Hole", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 5, "M", "Red", "A_Hole", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 5, "M", "Red", "A_Hole", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 5, "M", "Red", "A_Hole", 1);
+INSERT INTO bears VALUES (5, "Melissa", 5, "M", "Red", "A_Hole", 0);
+INSERT INTO bears VALUES (6, "Grinch", 5, "M", "Red", "A_Hole", 1);
+INSERT INTO bears VALUES (7, "Wendy", 5, "M", "Red", "A_Hole", 0);
+INSERT INTO bears VALUES (8, NULL, 5, "M", "Red", "A_Hole", 1);
+
